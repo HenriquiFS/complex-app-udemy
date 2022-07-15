680e0f92a4f9dcc6a5be6e2a0ecb49634822192e
Henriqui Ferreira Segantini <henriqui51298@gmail.com>
2022-07-15 15:13:21 -0300

    Terceiro commit, adicionando o Router e os Controllers

---
+++ controllers/userController.js
@@ -0,0 +1,15 @@
+exports.home = (req, res) => {
+  res.render('home-guest')
+}
+
+exports.login = (req, res) => {
+
+}
+
+exports.logout = (req, res) => {
+  
+}
+
+exports.register = (req, res) => {
+  res.send("Test 1 2 3")
+}
