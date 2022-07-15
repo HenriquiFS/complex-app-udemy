305376830dcdab5a9165c137e1527c142995c7a7
Henriqui Ferreira Segantini <henriqui51298@gmail.com>
2022-07-15 14:31:36 -0300

    Segundo commit, adicionando o front end da primeira página

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'))
+app.set('views', 'views')
+app.set('view engine', 'ejs')
+
 app.get('/', (req,res) => {
-  res.send("Testing...")
+  res.render('home-guest')
 })
 
 app.listen(3000, () => console.log('Server started at http://localhost:3000') );
