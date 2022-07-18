596e4389387ec427035a51f3da489302cafbd683
Henriqui Ferreira Segantini <henriqui51298@gmail.com>
2022-07-18 18:16:53 -0300

    Oitavo commit, adicionando e visualizando posts

--- router.js
+++ router.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('./controllers/userController')
+const userController = require('./controllers/userController');
+const postController = require('./controllers/postController');
 
+//USER RELATED ROUTES
 router.get('/', userController.home)
 router.post('/register', userController.register)
 router.post('/login', userController.login)
 router.post('/logout', userController.logout)
+
+//POST RELATED ROUTES 
+router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen)
+router.post('/create-post', userController.mustBeLoggedIn, postController.create)
+router.get('/post/:id', postController.viewSingle, )
 
 module.exports = router;
